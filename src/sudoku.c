bc18a6cc6e1c6de77530e718ce453cf69a9d86cf
Lookins01 <lookins1q@gmail.com>
2024-10-18 06:44:44 +0500

    [feat] change the way themes stored

--- src/sudoku.c
+++ src/sudoku.c
@@ -27,22 +27,6 @@
 #define OC_WIDTH  ((CELL_SIZE * FIELD_WIDTH) + (SEP_SIZE * SEP_COUNT) + (CELL_SEP_COUNT * CELL_SEP_SIZE) + 0)
 #define OC_HEIGHT ((CELL_SIZE * FIELD_HEIGHT) + (SEP_SIZE * SEP_COUNT) + (CELL_SEP_COUNT * CELL_SEP_SIZE) + TOP_MARGIN)
 
-
-int32_t BACKGROUND_COLOR    = (FRAPPE_BASE);
-int32_t SEP_COLOR           = (FRAPPE_SURFACE2);
-int32_t CELL_SEP_COLOR      = (FRAPPE_SURFACE1);
-int32_t CURSOR_COLOR        = (FRAPPE_OVERLAY2);
-int32_t CURSOR_SIMILAR      = (FRAPPE_OVERLAY1);
-int32_t SUB_CURSOR_COLOR    = (FRAPPE_OVERLAY0);
-int32_t BASE_TEXT_COLOR     = (FRAPPE_ROSEWATER);
-int32_t SELECTED_TEXT_COLOR = (FRAPPE_ROSEWATER);
-int32_t WIN_TEXT_COLOR      = (FRAPPE_GREEN);
-int32_t LOSE_TEXT_COLOR     = (FRAPPE_RED);
-int32_t HEART_CONTOUR       = (FRAPPE_CRUST);
-int32_t HEART_RED           = (FRAPPE_RED);
-int32_t LAVENDER            = (FRAPPE_LAVENDER);
-int32_t MANTLE              = (FRAPPE_MANTLE);
-
 #define DIGIT_1   1
 #define DIGIT_2   2
 #define DIGIT_3   3
@@ -59,7 +43,7 @@ int32_t MANTLE              = (FRAPPE_MANTLE);
 
 int rand(void);
 
-static uint32_t pixels[OC_WIDTH*OC_HEIGHT];
+uint32_t pixels[OC_WIDTH*OC_HEIGHT];
 
 static int field[FIELD_WIDTH*FIELD_HEIGHT] = {0};
 static int visible_field[FIELD_WIDTH*FIELD_HEIGHT] = {0};
@@ -74,6 +58,25 @@ typedef enum {
     MOCHA
 } Colorscheme;
 
+typedef enum {
+    BACKGROUND_COLOR = 0,
+    SEP_COLOR,
+    CELL_SEP_COLOR,
+    CURSOR_COLOR,
+    CURSOR_SIMILAR,
+    SUB_CURSOR_COLOR,
+    BASE_TEXT_COLOR,
+    SELECTED_TEXT_COLOR,
+    WIN_TEXT_COLOR,
+    LOSE_TEXT_COLOR,
+    HEART_CONTOUR,
+    HEART_RED,
+    LAVENDER,
+    MANTLE
+} Colors;
+
+uint32_t *themes[] = {frappe_theme, latte_theme, macchiato_theme, mocha_theme}; 
+uint32_t *theme = frappe_theme;
 
 typedef enum {
     EASY = 0,
@@ -254,71 +257,9 @@ void change_difficulty(Difficulty dif)
     init_game();
 }
 
-// TODO: Rewrite this shit
 void change_colorscheme(Colorscheme cs)
 {
-    if (cs == FRAPPE) {
-        BACKGROUND_COLOR    = (FRAPPE_BASE);
-        SEP_COLOR           = (FRAPPE_SURFACE2);
-        CELL_SEP_COLOR      = (FRAPPE_SURFACE1);
-        CURSOR_COLOR        = (FRAPPE_OVERLAY2);
-        CURSOR_SIMILAR      = (FRAPPE_OVERLAY1);
-        SUB_CURSOR_COLOR    = (FRAPPE_OVERLAY0);
-        BASE_TEXT_COLOR     = (FRAPPE_ROSEWATER);
-        SELECTED_TEXT_COLOR = (FRAPPE_ROSEWATER);
-        WIN_TEXT_COLOR      = (FRAPPE_GREEN);
-        LOSE_TEXT_COLOR     = (FRAPPE_RED);
-        HEART_CONTOUR       = (FRAPPE_CRUST);
-        HEART_RED           = (FRAPPE_RED);
-        LAVENDER            = (FRAPPE_LAVENDER);
-        MANTLE              = (FRAPPE_MANTLE);
-    } else if (cs == LATTE) {
-        BACKGROUND_COLOR    = (LATTE_BASE);
-        SEP_COLOR           = (LATTE_SURFACE2);
-        CELL_SEP_COLOR      = (LATTE_SURFACE1);
-        CURSOR_COLOR        = (LATTE_OVERLAY2);
-        CURSOR_SIMILAR      = (LATTE_OVERLAY1);
-        SUB_CURSOR_COLOR    = (LATTE_OVERLAY0);
-        BASE_TEXT_COLOR     = (LATTE_ROSEWATER);
-        SELECTED_TEXT_COLOR = (LATTE_ROSEWATER);
-        WIN_TEXT_COLOR      = (LATTE_GREEN);
-        LOSE_TEXT_COLOR     = (LATTE_RED);
-        HEART_CONTOUR       = (LATTE_CRUST);
-        HEART_RED           = (LATTE_RED);
-        LAVENDER            = (LATTE_LAVENDER);
-        MANTLE              = (LATTE_MANTLE);
-    } else if (cs == MOCHA) {
-        BACKGROUND_COLOR    = (MOCHA_BASE);
-        SEP_COLOR           = (MOCHA_SURFACE2);
-        CELL_SEP_COLOR      = (MOCHA_SURFACE1);
-        CURSOR_COLOR        = (MOCHA_OVERLAY2);
-        CURSOR_SIMILAR      = (MOCHA_OVERLAY1);
-        SUB_CURSOR_COLOR    = (MOCHA_OVERLAY0);
-        BASE_TEXT_COLOR     = (MOCHA_ROSEWATER);
-        SELECTED_TEXT_COLOR = (MOCHA_ROSEWATER);
-        WIN_TEXT_COLOR      = (MOCHA_GREEN);
-        LOSE_TEXT_COLOR     = (MOCHA_RED);
-        HEART_CONTOUR       = (MOCHA_CRUST);
-        HEART_RED           = (MOCHA_RED);
-        LAVENDER            = (MOCHA_LAVENDER);
-        MANTLE              = (MOCHA_MANTLE);
-    } else if (cs == MACCHIATO) {
-        BACKGROUND_COLOR    = (MACCHIATO_BASE);
-        SEP_COLOR           = (MACCHIATO_SURFACE2);
-        CELL_SEP_COLOR      = (MACCHIATO_SURFACE1);
-        CURSOR_COLOR        = (MACCHIATO_OVERLAY2);
-        CURSOR_SIMILAR      = (MACCHIATO_OVERLAY1);
-        SUB_CURSOR_COLOR    = (MACCHIATO_OVERLAY0);
-        BASE_TEXT_COLOR     = (MACCHIATO_ROSEWATER);
-        SELECTED_TEXT_COLOR = (MACCHIATO_ROSEWATER);
-        WIN_TEXT_COLOR      = (MACCHIATO_GREEN);
-        LOSE_TEXT_COLOR     = (MACCHIATO_RED);
-        HEART_CONTOUR       = (MACCHIATO_CRUST);
-        HEART_RED           = (MACCHIATO_RED);
-        LAVENDER            = (MACCHIATO_LAVENDER);
-        MANTLE              = (MACCHIATO_MANTLE);
-    }
-
+    theme = themes[cs];
 }
 
 // TODO: Rewrite this shit
@@ -331,11 +272,11 @@ void render_play_screen(Olivec_Canvas *oc)
         uint32_t heart_pixels[HEART_WIDTH*HEART_HEIGHT];
         for (int i = 0; i < HEART_HEIGHT*HEART_WIDTH; i++)
             if (heart_template[i] == 0x00000000)
-                heart_pixels[i] = BACKGROUND_COLOR;
+                heart_pixels[i] = theme[BACKGROUND_COLOR];
             else if (heart_template[i] == 0xFF000000)
-                heart_pixels[i] = HEART_CONTOUR;
+                heart_pixels[i] = theme[HEART_CONTOUR];
             else if (heart_template[i] == 0xFF0000FF)
-                heart_pixels[i] = HEART_RED;
+                heart_pixels[i] = theme[HEART_RED];
 
         Olivec_Canvas heart_sprite = olivec_canvas(heart_pixels, HEART_WIDTH, HEART_HEIGHT, HEART_WIDTH);
         int start_x = 5;
@@ -360,14 +301,14 @@ void render_play_screen(Olivec_Canvas *oc)
         {
             int hor_square = i / 3;
             int x = hor_square * SQUARE_SIZE + i % 3 * (CELL_SIZE + CELL_SEP_SIZE) + SEP_SIZE;
-            olivec_rect((*oc), x, cursor_y, CELL_SIZE, CELL_SIZE, SUB_CURSOR_COLOR);
+            olivec_rect((*oc), x, cursor_y, CELL_SIZE, CELL_SIZE, theme[SUB_CURSOR_COLOR]);
         }
 
         for (int i = 0; i < FIELD_HEIGHT; i++)
         {
             int vert_square = i / 3;
             int y = vert_square * SQUARE_SIZE + i % 3 * (CELL_SIZE + CELL_SEP_SIZE) + SEP_SIZE + TOP_MARGIN;
-            olivec_rect((*oc), cursor_x, y, CELL_SIZE, CELL_SIZE, SUB_CURSOR_COLOR);
+            olivec_rect((*oc), cursor_x, y, CELL_SIZE, CELL_SIZE, theme[SUB_CURSOR_COLOR]);
         }
 
         int start_line = cursor_line - cursor_line % 3,
@@ -380,7 +321,7 @@ void render_play_screen(Olivec_Canvas *oc)
                 int hor_square = column / 3;
                 int x = hor_square * SQUARE_SIZE + column % 3 * (CELL_SIZE + CELL_SEP_SIZE) + SEP_SIZE;
                 int y = vert_square * SQUARE_SIZE + line % 3 * (CELL_SIZE + CELL_SEP_SIZE) + SEP_SIZE + TOP_MARGIN;
-                olivec_rect((*oc), x, y, CELL_SIZE, CELL_SIZE, SUB_CURSOR_COLOR);
+                olivec_rect((*oc), x, y, CELL_SIZE, CELL_SIZE, theme[SUB_CURSOR_COLOR]);
             }
         }
 
@@ -399,10 +340,10 @@ void render_play_screen(Olivec_Canvas *oc)
                 int hor_square = column / 3;
                 int x = hor_square * SQUARE_SIZE + column % 3 * (CELL_SIZE + CELL_SEP_SIZE) + SEP_SIZE;
                 int y = vert_square * SQUARE_SIZE + line % 3 * (CELL_SIZE + CELL_SEP_SIZE) + SEP_SIZE + TOP_MARGIN;
-                olivec_rect((*oc), x, y, CELL_SIZE, CELL_SIZE, CURSOR_SIMILAR);
+                olivec_rect((*oc), x, y, CELL_SIZE, CELL_SIZE, theme[CURSOR_SIMILAR]);
             }
         }
-        olivec_rect((*oc), cursor_x, cursor_y, CELL_SIZE, CELL_SIZE, CURSOR_COLOR);
+        olivec_rect((*oc), cursor_x, cursor_y, CELL_SIZE, CELL_SIZE, theme[CURSOR_COLOR]);
     }
 
     /* draw numbers */
@@ -410,7 +351,7 @@ void render_play_screen(Olivec_Canvas *oc)
     int font_height = olivec_default_font.height * font_size;
     int font_width = olivec_default_font.width * font_size;
     char *labels[10] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-    uint32_t text_colors[2] = {BASE_TEXT_COLOR, SELECTED_TEXT_COLOR};
+    uint32_t text_colors[2] = {theme[BASE_TEXT_COLOR], theme[SELECTED_TEXT_COLOR]};
 
     for (int line = 0; line < FIELD_HEIGHT; line++)
     {
@@ -439,11 +380,11 @@ void render_play_screen(Olivec_Canvas *oc)
             /* vertical */
             int x = i * (CELL_SEP_SIZE + CELL_SIZE) + (SEP_SIZE + CELL_SIZE) + s * SQUARE_SIZE;
             int height = (FIELD_HEIGHT * CELL_SIZE) + (SEP_COUNT * SEP_SIZE) + (CELL_SEP_COUNT * CELL_SEP_SIZE);
-            olivec_rect((*oc), x, TOP_MARGIN, CELL_SEP_SIZE, height, CELL_SEP_COLOR);
+            olivec_rect((*oc), x, TOP_MARGIN, CELL_SEP_SIZE, height, theme[CELL_SEP_COLOR]);
             /* horizontal */
             int y = i * (CELL_SEP_SIZE + CELL_SIZE) + (SEP_SIZE + CELL_SIZE) + s * (3 * CELL_SIZE + SEP_SIZE + 2 * CELL_SEP_SIZE) + TOP_MARGIN;
             int width= (FIELD_HEIGHT * CELL_SIZE) + (SEP_COUNT * SEP_SIZE) + (CELL_SEP_COUNT * CELL_SEP_SIZE);
-            olivec_rect((*oc), 0, y, width, CELL_SEP_SIZE, CELL_SEP_COLOR);
+            olivec_rect((*oc), 0, y, width, CELL_SEP_SIZE, theme[CELL_SEP_COLOR]);
         }
     }
 
@@ -453,41 +394,41 @@ void render_play_screen(Olivec_Canvas *oc)
         /* vertical */
         int x = i * ((CELL_SIZE * 3) + SEP_SIZE + (CELL_SEP_SIZE * 2));
         int height = (FIELD_HEIGHT * CELL_SIZE) + (SEP_COUNT * SEP_SIZE) + (CELL_SEP_COUNT * CELL_SEP_SIZE);
-        olivec_rect((*oc), x, TOP_MARGIN, SEP_SIZE, height, SEP_COLOR);
+        olivec_rect((*oc), x, TOP_MARGIN, SEP_SIZE, height, theme[SEP_COLOR]);
         /* horizontal */
         int y = i * ((CELL_SIZE * 3) + SEP_SIZE + (CELL_SEP_SIZE * 2)) + TOP_MARGIN;
         int width = (FIELD_WIDTH * CELL_SIZE) + (SEP_COUNT * SEP_SIZE) + (CELL_SEP_COUNT * CELL_SEP_SIZE);
-        olivec_rect((*oc), 0, y, width, SEP_SIZE, SEP_COLOR);
+        olivec_rect((*oc), 0, y, width, SEP_SIZE, theme[SEP_COLOR]);
     }
 }
 
 void render_win_screen(Olivec_Canvas *oc) {
         int width = OC_WIDTH;
         int height = 200;
-        olivec_rect((*oc), (OC_WIDTH - width) / 2, (OC_HEIGHT - height) / 2, width, height, MANTLE);
-        olivec_frame((*oc), (OC_WIDTH - width) / 2, (OC_HEIGHT - height) / 2, width, height, 5, LAVENDER);
+        olivec_rect((*oc), (OC_WIDTH - width) / 2, (OC_HEIGHT - height) / 2, width, height, theme[MANTLE]);
+        olivec_frame((*oc), (OC_WIDTH - width) / 2, (OC_HEIGHT - height) / 2, width, height, 5, theme[LAVENDER]);
 
         int font_size = 5;
         const char *win_text = "You win :)";
         Olivec_Vector2 text_size = olivec_measure_text(win_text, sudoku_font, font_size);
-        olivec_text((*oc), win_text, (OC_WIDTH - text_size.x) /2 , (OC_HEIGHT - text_size.y) / 2, sudoku_font, font_size, WIN_TEXT_COLOR);
+        olivec_text((*oc), win_text, (OC_WIDTH - text_size.x) /2 , (OC_HEIGHT - text_size.y) / 2, sudoku_font, font_size, theme[WIN_TEXT_COLOR]);
 }
 
 void render_lose_screen(Olivec_Canvas *oc) {
         int width = OC_WIDTH;
         int height = OC_HEIGHT;
-        olivec_frame((*oc), 0 + (float)5/2, (OC_HEIGHT - height) / 2, width - 1, height, 5, LAVENDER);
+        olivec_frame((*oc), 0 + (float)5/2, (OC_HEIGHT - height) / 2, width - 1, height, 5, theme[LAVENDER]);
 
         int font_size = 5;
         const char *lose_text = "You lose :(";
         Olivec_Vector2 text_size = olivec_measure_text(lose_text, sudoku_font, font_size);
-        olivec_text((*oc), "You lose:(", (OC_WIDTH - text_size.x) /2 , (OC_HEIGHT - text_size.y) / 2, sudoku_font, font_size, LOSE_TEXT_COLOR);
+        olivec_text((*oc), "You lose:(", (OC_WIDTH - text_size.x) /2 , (OC_HEIGHT - text_size.y) / 2, sudoku_font, font_size, theme[LOSE_TEXT_COLOR]);
 }
 
 Olivec_Canvas render_game(void)
 {
     Olivec_Canvas oc = olivec_canvas(pixels, OC_WIDTH, OC_HEIGHT, OC_WIDTH);
-    olivec_fill(oc, BACKGROUND_COLOR);
+    olivec_fill(oc, theme[BACKGROUND_COLOR]);
 
     switch (state) {
         case PLAY: {
